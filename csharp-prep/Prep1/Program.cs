813ea59acf22464ad0d816fefba6b882fc5790cb
eldersud <cua20002@byui.edu>
2023-01-07 13:32:01 -0500

    indentation and spaces corrected

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,8 +7,10 @@ class Program
         //week One - activity 1
         Console.Write("What is your first name? ");
         string name = Console.ReadLine();
+        
         Console.Write("What is your last name? ");
         string lname = Console.ReadLine();
+        
         Console.WriteLine($"Your name is {lname}, {name} {lname}.");
 
     }
