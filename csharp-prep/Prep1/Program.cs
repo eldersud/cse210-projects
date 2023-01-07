56242948847a961884576929aa268b9002eede98
eldersud <cua20002@byui.edu>
2023-01-07 13:25:51 -0500

    week 1 first activity finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //week One - activity 1
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lname}, {name} {lname}.");
+
     }
 }
