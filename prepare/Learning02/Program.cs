1431221cc4f5b2d9229f28db9959178b6d61cae4
eldersud <cua20002@byui.edu>
2023-01-18 22:45:15 -0500

    week 3 assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTittle="Designer";
+        job1._company="Go Fitness center";
+        job1._startYear=2021;
+        job1._endYear=2022;
+
+        Job job2 = new Job();
+        job2._jobTittle="Gym Instructor";
+        job2._company="U-fit";
+        job2._startYear=2016;
+        job2._endYear=2019;
+
+        Resume myResume = new Resume();
+        myResume._name="Elder Cuases";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayJobDetails();
+
+        
     }
 }
