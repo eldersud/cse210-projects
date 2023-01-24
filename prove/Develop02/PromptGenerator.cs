59e9119814b905bf526207924e1b28bc0428909d
eldersud <cua20002@byui.edu>
2023-01-24 02:04:51 -0500

    keep working

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,5 +1,22 @@
 using System;
+using System.IO;
 public class PromtGenerator
 {
+  public string DisplayPrompot(){
+       int npreguntas = 1;
+			string path = @"/Users/eldercuases/Documents/BYU 2023/CSE 210/cse210-projects/prove/develop02/prompt.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                npreguntas++;
+            }
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, npreguntas);
+            //Console.WriteLine(number);
+            return(lines[number-1]);
+
+        }
     
-}
+
+  
+} 
